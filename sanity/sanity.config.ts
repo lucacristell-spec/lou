37e9e8e9190d67b28057c1054bc0dd5934f9d02b
gianLuca Cristell <gianlucacristell@gianLucas-MacBook-Pro.local>
2026-03-03 14:37:38 +0700

    Fix: Correct Sanity Project ID (08plez70)

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -3,7 +3,7 @@ import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "08plez70";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineConfig({
